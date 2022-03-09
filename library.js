98eb6e14eb8372655f83509669cf2a039827830e
Oscar <squatasbigbird@protonmail.com>
2022-03-08 20:11:30 -0500

    added better button functionality like toggle buttons

--- library.js
+++ library.js
@@ -1,13 +1,12 @@
-const form = document.querySelector('form');
+const form = document.querySelector('.form-container');
 const btn = document.querySelector('.newbook');
 const closer = document.querySelector('.close');
-btn.onclick  = () => {form.style.display = "block"};
-closer.onclick = () => {form.style.display = "none"}; 
-window.onclick = function(event) {
-    if (event.target == window) {
-      form.style.display = "none";
-    }
-  }
+const backdrop = document.querySelector('.backdrop');
+function removeForm() {form.classList.remove('open')};
+function addForm() {form.classList.add('open')};
+closer.onclick = () => {removeForm()}; 
+btn.onclick = () => {addForm();}
+backdrop.onclick = () => {removeForm();}
 let myLibrary = [
     // {title : 'Watchmen',
     //  author : 'Alan Moore',
@@ -26,47 +25,70 @@ let myLibrary = [
     // }
     
 ];
+const submit = document.querySelector('.Submit');
+submit.onclick = () => {
+    removeForm();
+    newBook();
+}
 function Book(title,author,num, read) {
     this.title = title;
     this.author = author;
     this.pages = num;
-    this.pages = read;
+    this.read = read;
 }
 function newBook() {
-    let formtitle = document.getElementsByClassName("title").value;
-    let formauthor = document.getElementsByClassName("author").value;
-    let formpages = document.getElementsByClassName("pages").value;
-    let formread = document.getElementsByClassName("read").value;
+    let formtitle = document.getElementsByClassName("title")[0].value;
+    let formauthor = document.getElementsByClassName("author")[0].value;
+    let formpages = document.getElementsByClassName("pages")[0].value;
+    let formread = document.getElementsByClassName("read")[0].value;
     let bool = false;
     if (formread.checked) {
         bool = true;
     }
     let book = new Book(formtitle,formauthor,formpages,bool);
-    addBookToLibrary(book);
-}
-// const newbook = document.querySelector(".newbook").onclick = () => {
-//  newBook();   
-// }
-function addBookToLibrary() {
     myLibrary.push(book);
+    displayBookOnShelf(myLibrary);
 }
 const shelf = document.querySelector(".book-container");
-function displayBookOnShelf() {
-    for(book in myLibrary) {
+function displayBookOnShelf(myLibrary) {
+    shelf.innerHTML = ''; //empty the parent 'book-container' class
+    for(let book in myLibrary) {
+       console.log(myLibrary[book]);
         let bookcover = document.createElement("div");
         bookcover.classList.add('book');
-        let title = document.createTextNode(`Title: + ${myLibrary[book].title}`);
-        let author = document.createTextNode(`Author: + ${myLibrary[book].author}`);
-        let pages = document.createTextNode(`Pages: +   ${myLibrary[book].num}`);
-        let read = document.createTextNode(`Read Status: + ${myLibrary[book].read}`);
+
+        let bookinfo = document.createElement("ul");
+        let title = document.createTextNode(`${myLibrary[book].title}`);
+        
+        let author = document.createTextNode(`by ${myLibrary[book].author}`);
+        let authorList = document.createElement("li");
+        authorList.classList.add('author');
+        authorList.appendChild(author);
+        
+        let pages = document.createTextNode(`Pages: ${myLibrary[book].pages}`);
+        let pagesList = document.createElement("li");
+        pagesList.classList.add('pages');
+        pagesList.appendChild(pages);
+        
+        let read = document.createTextNode(`Read Status: ${myLibrary[book].read}`);
+        let readStatus = document.createElement("li");
+        readStatus.classList.add('read-status');
+        readStatus.appendChild(read);
+        
         let h3el = document.createElement("h3");
         h3el.appendChild(title);
+        bookinfo.appendChild(authorList);
+        bookinfo.appendChild(pagesList);
+        bookinfo.appendChild(readStatus);
         bookcover.appendChild(h3el);
-        bookcover.appendChild(author);
-        bookcover.appendChild(pages);
-        bookcover.appendChild(read);
+        bookcover.appendChild(bookinfo);
         shelf.appendChild(bookcover);
-
-
+        // resetForm();
     }
+}
+function resetForm() {
+    document.getElementsByClassName('title')[0].value = '';
+    document.getElementsByClassName('author')[0].value = '';
+    document.getElementsByClassName('pages')[0].value = '';
+    document.getElementsByClassName('read')[0].value = false;
 }
